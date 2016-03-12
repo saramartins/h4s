1d7aac4d4f8a0c7e2d56f16b07c8dff0d0d75884
saramartins <sarma772@student.liu.se>
2016-03-13 00:52:10 +0100

    la till flera sökningar

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function myFunction(xml) {
   var xmlDoc = xml.responseXML;
   //var xmlDoc1 = xmlhttp1.responseXML;
 
-
+/*
   var table="<tr><th>Annonsrubrik</th><th>Yrkesbenämning</th><th>Kommun</th><th>Länk</th></tr>";
   var kommun = "";
   var yrke = "";
@@ -29,7 +29,7 @@ function myFunction(xml) {
   var x = xmlDoc.getElementsByTagName("matchningdata");
   //var annonsId = xmlDoc.getElementsByTagName("matchningdata");
 
-/*
+
   for (i = 0; i <x.length; i++) { 
 
     kommun += " " + x[i].getElementsByTagName("kommunnamn")[0].childNodes[0].nodeValue;
@@ -39,8 +39,6 @@ function myFunction(xml) {
   
 
   }
-*/
-
 
   kommun += " " + x[3].getElementsByTagName("kommunnamn")[0].childNodes[0].nodeValue;
   yrke += " " + x[3].getElementsByTagName("annonsrubrik")[0].childNodes[0].nodeValue;
@@ -50,6 +48,53 @@ function myFunction(xml) {
   document.getElementById("kommun").innerHTML = kommun;
   document.getElementById("yrke").innerHTML = yrke;
   document.getElementById("url").innerHTML = url;
+
+*/
+var kommun = "";
+  var yrke = "";
+  var url = "";
+  var kommun1 = "";
+  var yrke1 = "";
+  var url1 = "";
+  var kommun2 = "";
+  var yrke2 = "";
+  var url2 = "";
+
+  var x = xmlDoc.getElementsByTagName("matchningdata");
+  //var annonsId = xmlDoc.getElementsByTagName("matchningdata");
+
+
+  kommun += " " + x[0].getElementsByTagName("kommunnamn")[0].childNodes[0].nodeValue;
+  yrke += " " + x[0].getElementsByTagName("annonsrubrik")[0].childNodes[0].nodeValue;
+  url += " " + x[0].getElementsByTagName("annonsurl")[0].childNodes[0].nodeValue;
+
+  //document.getElementById("demo").innerHTML = table;
+  document.getElementById("kommun").innerHTML = kommun;
+  document.getElementById("yrke").innerHTML = yrke;
+  document.getElementById("url").innerHTML = url;
+
+
+
+  kommun1 += " " + x[1].getElementsByTagName("kommunnamn")[0].childNodes[0].nodeValue;
+  yrke1 += " " + x[1].getElementsByTagName("annonsrubrik")[0].childNodes[0].nodeValue;
+  url1 += " " + x[1].getElementsByTagName("annonsurl")[0].childNodes[0].nodeValue;
+
+  //document.getElementById("demo").innerHTML = table;
+  document.getElementById("kommun1").innerHTML = kommun1;
+  document.getElementById("yrke1").innerHTML = yrke1;
+  document.getElementById("url1").innerHTML = url1;
+
+
+  kommun2 += " " + x[2].getElementsByTagName("kommunnamn")[0].childNodes[0].nodeValue;
+  yrke2 += " " + x[2].getElementsByTagName("annonsrubrik")[0].childNodes[0].nodeValue;
+  url2 += " " + x[2].getElementsByTagName("annonsurl")[0].childNodes[0].nodeValue;
+
+  //document.getElementById("demo").innerHTML = table;
+  document.getElementById("kommun2").innerHTML = kommun2;
+  document.getElementById("yrke2").innerHTML = yrke2;
+  document.getElementById("url2").innerHTML = url2;
+
+
 
 }
 /*
