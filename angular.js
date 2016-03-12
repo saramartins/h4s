5ada5aba78009077bb87c09f0fe43aea8ddb7352
saramartins <sarma772@student.liu.se>
2016-03-12 22:21:00 +0100

    fic

--- angular.js
+++ angular.js
@@ -15,6 +15,8 @@
         templateUrl : 'results.html',
         controller  : 'resultsController'
       });
+
+  });
   // create the controller and inject Angular's $scope
   myApp.controller('mainController', function($scope) {
     // create a message to display in our view
