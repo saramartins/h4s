655d600d7a503114bac9586c3e4449b06136a287
kristinback <kristin.s.back@gmail.com>
2016-03-12 18:31:33 +0100

    data for each

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function loadXMLDoc() {
     //&& xmlhttp1.readyState == 4 && xmlhttp1.status == 200
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200 ) {
       myFunction(xmlhttp);
-      getNumApartment(xmlhttp1);
     }
   };
   xmlhttp.open("GET", "data/api_match_plats.xml", true);
@@ -23,27 +22,27 @@ function myFunction(xml) {
 
 
   var table="<tr><th>Annonsrubrik</th><th>Yrkesbenämning</th><th>Kommun</th><th>Länk</th></tr>";
+  var kommun = "Kommun:     ";
+  var yrke = "Yrke:      ";
 
   var x = xmlDoc.getElementsByTagName("matchningdata");
   //var annonsId = xmlDoc.getElementsByTagName("matchningdata");
 
 
   for (i = 0; i <x.length; i++) { 
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("annonsrubrik")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("arbetsplatsnamn")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("kommunnamn")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("annonsurl")[0].childNodes[0].nodeValue +
-    "</td></tr>";
 
-    //annonsId[i].getElementsByTagName("annonsid")[0].childNodes[0].nodeValue;
+    kommun += " " + x[i].getElementsByTagName("kommunnamn")[0].childNodes[0].nodeValue;
+    yrke += " " + x[i].getElementsByTagName("annonsrubrik")[0].childNodes[0].nodeValue;
+
+
+  
 
   }
 
-  document.getElementById("demo").innerHTML = table;
+
+  //document.getElementById("demo").innerHTML = table;
+  document.getElementById("kommun").innerHTML = kommun;
+  document.getElementById("yrke").innerHTML = yrke;
 
 }
 /*
