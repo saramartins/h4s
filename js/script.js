77f6575634e887655758d79172c8cfbcc9fe3c02
saramartins <sarma772@student.liu.se>
2016-03-13 08:16:18 +0100

    la till ikoner

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ var kommun = "";
   //document.getElementById("demo").innerHTML = table;
   document.getElementById("kommun").innerHTML = kommun;
   document.getElementById("yrke").innerHTML = yrke;
-
+  document.getElementById("yrkeom").innerHTML = yrke;
 
 
   kommun1 += " " + x[1].getElementsByTagName("kommunnamn")[0].childNodes[0].nodeValue;
@@ -46,7 +46,7 @@ var kommun = "";
   //document.getElementById("demo").innerHTML = table;
   document.getElementById("kommun1").innerHTML = kommun1;
   document.getElementById("yrke1").innerHTML = yrke1;
-
+  document.getElementById("yrke1om").innerHTML = yrke1;
 
   kommun2 += " " + x[2].getElementsByTagName("kommunnamn")[0].childNodes[0].nodeValue;
   yrke2 += " " + x[2].getElementsByTagName("annonsrubrik")[0].childNodes[0].nodeValue;
